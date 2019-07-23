3f574d662fd40762f15dd7780125e3c3a5fc2d8f
Mirko Mälicke <mirko.maelicke@kit.edu>
2019-07-23 08:17:44 +0200

    added configuration.js

---
+++ configuration.js
@@ -0,0 +1,38 @@
+var Config = {
+    title: "Sample Lecture",
+    navigation: [
+        {
+            label: "Home",
+            link: "index.md",
+            type: "static"
+        },
+        {
+            label: "Notebooks",
+            navigation: [
+                {
+                    label: "Introduction",
+                    link: "Introduction.html",
+                    github: "https://github.com/mmaelicke/sample-lecture/blob/master/Introduction.ipynb",
+                    binder: "https://mybinder.org/v2/gh/mmaelicke/sample-lecture/master?filepath=Introduction.ipynb"
+                }
+            ]
+        },
+        {
+            label: "Presentations",
+            navigation: [
+                {
+                    label: "Introduction",
+                    link: "Introduction.slides.html"
+                }
+            ]
+        },
+        {
+            label: "Exercises",
+            navigation: []
+        },
+        {
+            label: "About",
+            link: "about.md"
+        }
+    ]
+};
